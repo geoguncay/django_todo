71450fbfaf40eb4ee921669740f89c8b94cd59a7
Geo Guncay <81765997+geoguncay@users.noreply.github.com>
2023-08-11 23:11:57 -0500

    add form template

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,15 @@
 """ Views for todo app"""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
 def home(request):
-    """ Home page view"""
-    return render(request, 'base.html')
+    """Home page view"""
+    return render(request, "task.html")
+
+
+def create_task(request):
+    """Create task view"""
+    print(request.POST)
+    redirect(home)
+    return render(request, "task.html")
